3b6e19ffd46ee2a24f38c45dfde031390c381546
one1run <one@macaucall.com>
2026-02-13 16:20:12 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+
+const CACHE_NAME = 'busan-guide-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './index.tsx',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@400;700&family=Noto+Sans+TC:wght@300;400;500&display=swap'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event - Stale-while-revalidate strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        if (networkResponse.ok && event.request.method === 'GET') {
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+          });
+        }
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
